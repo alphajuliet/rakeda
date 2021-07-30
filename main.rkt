2087e37e9065c289d919f2ea29f76991d04d5dae
alphajuliet <andrew@alphajuliet.com>
2021-07-30 15:33:45 +1000

    Rename functions from r:... to r/... to align better with Racket practices. Rename r/contains? to r/in?, and r/find to r/find-in. Add curried r/eq. Update unit tests to match.

--- main.rkt
+++ main.rkt
@@ -14,96 +14,97 @@
 (define-syntax-rule (define/curry (fn args ...) body)
   (define fn (curry (λ (args ...) body))))
 
-(define r:curry (curry (λ (f a b) (f a b))))
-(define r:curryN (curry (λ (f . args) (apply f args))))
-#;(define r:c (curry (λ (f x . y) (apply f (cons x y)))))
+(define r/curry (curry (λ (f a b) (f a b))))
+(define r/curryN (curry (λ (f . args) (apply f args))))
+#;(define r/c (curry (λ (f x . y) (apply f (cons x y)))))
 
 ; Threading function
-; e.g. (>> 5 (r:add 2)) => 7
+; e.g. (>> 5 (r/add 2)) => 7
 ; Can also use ~> and ~>> from the theading package
 (define (>> it . fs)
   ((apply compose (reverse fs)) it))
 
 (define (true? x) (if x #t #f))
 (define (false? x) (if x #f #t))
+(define r/eq? (r/curry eq?))
 
 ; Reverse two arguments
-(define/curry (r:flip f a b)
+(define/curry (r/flip f a b)
   (apply f (list b a)))
-(define r:head first)
-(define r:last last)
-(define r:nth (r:flip list-ref))
-(define r:index-of (r:flip index-of))
-(define r:index-where (r:flip index-where))
+(define r/head first)
+(define r/last last)
+(define r/nth (r/flip list-ref))
+(define r/index-of (r/flip index-of))
+(define r/index-where (r/flip index-where))
 
 ; Direct mapping
-(define r:map (r:curry map))
-(define r:filter (r:curry filter))
-(define r:group-by (r:curry group-by))
-(define r:reduce (r:curryN foldl))
-(define r:reduce-right (r:curryN foldr))
-(define/curry (r:juxt fs x) (map (λ (f) (f x)) fs))
+(define r/map (r/curry map))
+(define r/filter (r/curry filter))
+(define r/group-by (r/curry group-by))
+(define r/reduce (r/curryN foldl))
+(define r/reduce-right (r/curryN foldr))
+(define/curry (r/juxt fs x) (map (λ (f) (f x)) fs))
 
 ; Zip functions
-(define r:zip (curry map list))
-(define/curry (r:zip-with fn lst1 lst2)
-  (map (curry apply fn) (r:zip lst1 lst2)))
-(define r:flatzip (compose flatten r:zip))
+(define r/zip (curry map list))
+(define/curry (r/zip-with fn lst1 lst2)
+  (map (curry apply fn) (r/zip lst1 lst2)))
+(define r/flatzip (compose flatten r/zip))
 
 ; Invert arguements
-(define r:take (r:curryN r:flip take))
-(define r:drop (r:curryN r:flip drop))
-(define r:sort (r:curryN r:flip sort))
+(define r/take (r/curryN r/flip take))
+(define r/drop (r/curryN r/flip drop))
+(define r/sort (r/curryN r/flip sort))
 
 ; Various list functions
 ; 1-argument
-(define r:flatten flatten)
-(define r:reverse reverse)
-(define r:uniq remove-duplicates)
-(define r:join (r:flip string-join))
+(define r/flatten flatten)
+(define r/reverse reverse)
+(define r/uniq remove-duplicates)
+(define r/join (r/flip string-join))
 
 ; 2-argument
-(define r:append (r:curry append))
-(define/curry (r:prepend x lst) (append (list x) lst))
-(define r:count (r:curry count))
-(define r:all (r:curry andmap))
-(define r:any (r:curry ormap))
-(define r:argmin (r:curry argmin))
-(define r:argmax (r:curry argmax))
+(define r/append (r/curry append))
+(define/curry (r/prepend x lst) (append (list x) lst))
+(define r/count (r/curry count))
+(define r/all (r/curry andmap))
+(define r/any (r/curry ormap))
+(define r/argmin (r/curry argmin))
+(define r/argmax (r/curry argmax))
 
-(define/curry (r:contains? x lst)
-  (if (findf (curry eq? x) lst) #t #f))
+(define/curry (r/in? x lst)
+  (if (findf (r/eq? x) lst) #t #f))
 
-(define/curry (r:find x lst)
+(define/curry (r/find-in x lst)
   (if (member x lst) x #f))
 
 ; List union and intersections
-(define r:union (compose r:uniq append))
-(define/curry (r:intersection lst1 lst2)
-  (r:uniq (r:filter (r:curryN r:flip r:contains? lst1) lst2)))
+(define r/union (compose r/uniq append))
+(define/curry (r/intersection lst1 lst2)
+  (r/uniq (r/filter (r/curryN r/flip r/in? lst1) lst2)))
 
 ; List rotations
-(define (r:rotate-left lst)
-  (r:append (cdr lst)
+(define (r/rotate-left lst)
+  (r/append (cdr lst)
             (list (car lst))))
-(define (r:rotate-right lst)
-  (r:append (list (r:last lst))
-            (r:take (sub1 (length lst)) lst)))
+(define (r/rotate-right lst)
+  (r/append (list (r/last lst))
+            (r/take (sub1 (length lst)) lst)))
 
-(define (r:iterate fn n x)
+(define (r/iterate fn n x)
 ;; Iterate a function over an argument
-;; (r:iterate fn n x) → '(x (fn x) (fn (fn x)) ... (fn^n x))
+;; (r/iterate fn n x) → '(x (fn x) (fn (fn x)) ... (fn^n x))
   (for/fold ([acc (list x)])
             ([i (range n)])
             (let ([y (fn (last acc))])
               (append acc (list y)))))
 
 ; Curried basic math functions
-(define r:+       (r:curry +))
-(define r:-       (r:curry -))
-(define r:*       (r:curry *))
-(define r:/       (r:curry /))
-(define r:expt    (r:curry expt))
-(define r:modulo  (r:curry modulo))
+(define r/+       (r/curry +))
+(define r/-       (r/curry -))
+(define r/*       (r/curry *))
+(define r/div     (r/curry /))
+(define r/expt    (r/curry expt))
+(define r/modulo  (r/curry modulo))
 
 ; The End
--- rakeda-test.rkt
+++ rakeda-test.rkt
@@ -10,98 +10,98 @@
   #:before
   (λ () #t)
 
-  (test-case "r:flip"
-             (check-equal? (r:flip - 10 4)
+  (test-case "r/flip"
+             (check-equal? (r/flip - 10 4)
                            -6))
-  (test-case "r:head r:last r:nth"
-             (check-equal? (r:head '(1 2 3)) 1)
-             (check-equal? (r:last '(1 2 3)) 3)
-             (check-equal? (r:nth 2 '(1 2 3)) 3)
-             (define f (r:nth 2))
+  (test-case "r/head r/last r/nth"
+             (check-equal? (r/head '(1 2 3)) 1)
+             (check-equal? (r/last '(1 2 3)) 3)
+             (check-equal? (r/nth 2 '(1 2 3)) 3)
+             (define f (r/nth 2))
              (check-equal? (f '(1 2 3)) 3)
-             (check-equal? (r:index-of 2 '(1 2 3)) 1))
+             (check-equal? (r/index-of 2 '(1 2 3)) 1))
   
   (test-case "Direct mappings"
-             (check-equal? (r:map add1 '(1 2 3))
+             (check-equal? (r/map add1 '(1 2 3))
                            '(2 3 4))
-             (define f (r:map add1))
+             (define f (r/map add1))
              (check-equal? (f '(1 2 3))
                            '(2 3 4))
-             (define g (r:filter odd?))
+             (define g (r/filter odd?))
              (check-equal? (g '(1 2 3))
                            '(1 3))
-             (check-equal? (r:reduce + 1 '(1 2 3)) 7))
-  (test-case "r:zip"
-             (check-equal? (r:zip '(1 2) '(3 4))
+             (check-equal? (r/reduce + 1 '(1 2 3)) 7))
+  (test-case "r/zip"
+             (check-equal? (r/zip '(1 2) '(3 4))
                            '((1 3) (2 4))))
-  (test-case "r:zip-with"
-             (check-equal? (r:zip-with + '(1 2) '(3 4))
+  (test-case "r/zip-with"
+             (check-equal? (r/zip-with + '(1 2) '(3 4))
                            '(4 6)))
-  (test-case "r:flatzip"
-             (check-equal? (r:flatzip '(1 2) '(3 4))
+  (test-case "r/flatzip"
+             (check-equal? (r/flatzip '(1 2) '(3 4))
                            '(1 3 2 4)))
-  #;(test-case "r:juxt"
-             (define fns '((r:add 1) (r:subtract 1)))
-             (check-equal? (r:juxt fns '(1 2 3)) '((2 3 4) (0 1 2))))
+  #;(test-case "r/juxt"
+             (define fns '((r/add 1) (r/subtract 1)))
+             (check-equal? (r/juxt fns '(1 2 3)) '((2 3 4) (0 1 2))))
   
-  (test-case "r:take r:drop"
-             (check-equal? (r:take 2 '(1 2 3))
+  (test-case "r/take r/drop"
+             (check-equal? (r/take 2 '(1 2 3))
                            '(1 2))
-             (check-equal? (r:drop 2 '(1 2 3 4))
+             (check-equal? (r/drop 2 '(1 2 3 4))
                            '(3 4)))
-  (test-case "r:any and r:all"
-             (check-equal? (r:any odd? '(1 2 3)) #t)
-             (define f (r:any odd?))
+  (test-case "r/any and r/all"
+             (check-equal? (r/any odd? '(1 2 3)) #t)
+             (define f (r/any odd?))
              (check-equal? (f '(2 4 6)) #f)
-             (check-equal? (r:all odd? '(1 2 3)) #f)
-             (check-equal? (r:all odd? '(1 3 5)) #t)) 
+             (check-equal? (r/all odd? '(1 2 3)) #f)
+             (check-equal? (r/all odd? '(1 3 5)) #t))
 
-  (test-case "r:sort"
-             (check-equal? (r:sort < '(3 2 1))
+  (test-case "r/sort"
+             (check-equal? (r/sort < '(3 2 1))
                            '(1 2 3)))
-  (test-case "r:uniq"
-             (check-equal? (r:uniq '(5 1 2 2 3 4 4))
+  (test-case "r/uniq"
+             (check-equal? (r/uniq '(5 1 2 2 3 4 4))
                            '(5 1 2 3 4)))
-  (test-case "r:prepend"
-             (check-equal? (r:prepend 1 '(2 3))
+  (test-case "r/prepend"
+             (check-equal? (r/prepend 1 '(2 3))
                            '(1 2 3)))
-  (test-case "r:contains"
-             (check-equal? (r:contains? 2 '(1 2 3)) #t)
-             (check-equal? (r:contains? 4 '(1 2 3)) #f))
+  (test-case "r/contains"
+             (check-equal? (r/in? 2 '(1 2 3)) #t)
+             (check-equal? (r/in? 4 '(1 2 3)) #f))
   
-  (test-case "r:find"
-             (check-equal? (r:find 2 '(1 2 3))
+  (test-case "r/find"
+             (check-equal? (r/find-in 2 '(1 2 3))
                            2)
-             (check-equal? (r:find 5 '(1 2 3))
+             (check-equal? (r/find-in 5 '(1 2 3))
                            #f))
-  (test-case "r:union"
-             (check-equal? (r:union '(1 2 3) '(2 3 4))
+  (test-case "r/union"
+             (check-equal? (r/union '(1 2 3) '(2 3 4))
                            '(1 2 3 4)))
-  (test-case "r:intersection"
-             (check-equal? (r:intersection '(1 2 3) '(2 3 4))
+  (test-case "r/intersection"
+             (check-equal? (r/intersection '(1 2 3) '(2 3 4))
                            '(2 3)))
-  (test-case "r:count and r:flatten"
-             (check-equal? (r:count odd? '(1 2 3)) 2)
-             (check-equal? (r:flatten '((1 2 (3) ((4) 5))))
+  (test-case "r/count and r/flatten"
+             (check-equal? (r/count odd? '(1 2 3)) 2)
+             (check-equal? (r/flatten '((1 2 (3) ((4) 5))))
                            '(1 2 3 4 5)))
 
   (test-case "Rotations"
-             (check-equal? (r:rotate-left '(1 2 3))
+             (check-equal? (r/rotate-left '(1 2 3))
                            '(2 3 1))
-             (check-equal? (r:rotate-right '(1 2 3))
+             (check-equal? (r/rotate-right '(1 2 3))
                            '(3 1 2)))
 
   (test-case "Iterate function application"
-             (check-equal? (r:iterate r:rotate-left 2 '(1 2 3))
+             (check-equal? (r/iterate r/rotate-left 2 '(1 2 3))
                            '((1 2 3) (2 3 1) (3 1 2))))
 
   (test-case "Math"
-             (check-equal? (r:+ 2 3) 5)
-             (define inc (r:+ 1))
+             (check-equal? (r/+ 2 3) 5)
+             (define inc (r/+ 1))
              (check-equal? (inc 2) 3))
 
   (test-case "Composition"
-             (define f (compose (r:+ 1) (r:* 3)))
+             (define f (compose (r/+ 1) (r/* 3)))
              (check-equal? (f 4) 13))
   )
 
